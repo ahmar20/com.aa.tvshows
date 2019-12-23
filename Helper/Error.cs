aefbb34117863c45cef3da473b8b9055bc2147f3
Ahmar Aftab <pakahmar@hotmail.com>
2019-12-23 18:54:28 +0500

    Add support for Snackbar and Toast notification for in-app errors

--- Helper/Error.cs
+++ Helper/Error.cs
@@ -9,31 +9,39 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using AndroidX.Core.Content;
+using Google.Android.Material.Snackbar;
 
 namespace com.aa.tvshows.Helper
 {
     public class Error
     {
-        public Error ErrorInstance { get => GetInstance(); }
-
-        private Error GetInstance()
+        private static Error instance;
+        public static Error Instance
         {
-            return new Error();
+            get
+            {
+                if (instance == null) instance = new Error();
+                return instance;
+            }
         }
 
-        public void SetInstance()
+        public Error()
         {
-
+            instance = new Error();
         }
 
-        public void ShowErrorTip(string message)
+        public void ShowErrorTip(string message, Context c)
         {
-
+            Toast.MakeText(c, message, ToastLength.Short).Show();
         }
 
-        public void ShowErrorSnack(string message)
+        public void ShowErrorSnack(string message, View v, Action actionCallback = default)
         {
-
+            Snackbar.Make(v, message, Snackbar.LengthIndefinite)
+                    .SetAction("OK", (s) => { actionCallback?.Invoke(); })
+                    .SetActionTextColor(ContextCompat.GetColor(v.Context, Resource.Color.colorPrimary))
+                    .Show();
         }
     }
 }
