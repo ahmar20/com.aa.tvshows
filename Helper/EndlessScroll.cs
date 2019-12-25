4a5f4a48c3dfd4b43e4cf11ee42adaaf968c546d
Ahmar Aftab <pakahmar@hotmail.com>
2019-12-25 14:59:22 +0500

    Fix endless scroll and improve performance

--- Helper/EndlessScroll.cs
+++ Helper/EndlessScroll.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -12,40 +13,131 @@ using AndroidX.RecyclerView.Widget;
 
 namespace com.aa.tvshows.Helper
 {
-    public class EndlessScroll : RecyclerView.OnScrollListener
+    public abstract class EndlessRecyclerViewScrollListener : RecyclerView.OnScrollListener
     {
-        private readonly LinearLayoutManager layoutManager;
-        private readonly Action loadMoreAction;
-        private int firstVisibleItemPosition = 0;
-        private int totalItemCount = 0;
-        private int visibleItemCount = 0;
-        private int previousTotal = 0;
         private readonly int visibleThreshold = 10;
+        // The current offset index of data you have loaded
+        private int currentPage = 0;
+        // The total number of items in the dataset after the last load
+        private int previousTotalItemCount = 0;
+        // True if we are still waiting for the last set of data to load.
+        private bool loading = true;
+        // Sets the starting page index
+        private readonly int startingPageIndex = 0;
 
-        public EndlessScroll(LinearLayoutManager layoutManager, Action loadMoreAction)
+        readonly RecyclerView.LayoutManager mLayoutManager;
+
+        public EndlessRecyclerViewScrollListener(LinearLayoutManager layoutManager)
         {
-            this.layoutManager = layoutManager;
-            this.loadMoreAction = loadMoreAction;
+            this.mLayoutManager = layoutManager;
+        }
+
+        public EndlessRecyclerViewScrollListener(GridLayoutManager layoutManager)
+        {
+            this.mLayoutManager = layoutManager;
+            visibleThreshold *= layoutManager.SpanCount;
+        }
+
+        public EndlessRecyclerViewScrollListener(StaggeredGridLayoutManager layoutManager)
+        {
+            this.mLayoutManager = layoutManager;
+            visibleThreshold *= layoutManager.SpanCount;
+        }
+
+        public int GetLastVisibleItem(int[] lastVisibleItemPositions)
+        {
+            int maxSize = 0;
+            for (int i = 0; i < lastVisibleItemPositions.Length; i++)
+            {
+                if (i == 0)
+                {
+                    maxSize = lastVisibleItemPositions[i];
+                }
+                else if (lastVisibleItemPositions[i] > maxSize)
+                {
+                    maxSize = lastVisibleItemPositions[i];
+                }
+            }
+            return maxSize;
         }
 
         public override void OnScrolled(RecyclerView recyclerView, int dx, int dy)
         {
-            base.OnScrolled(recyclerView, dx, dy);
+            int lastVisibleItemPosition = 0;
+            int totalItemCount = mLayoutManager.ItemCount;
 
-            if (dy > 0)
+            if (mLayoutManager is StaggeredGridLayoutManager)
             {
-                visibleItemCount = layoutManager.ChildCount;
-                totalItemCount = layoutManager.ItemCount;
-                firstVisibleItemPosition = layoutManager.FindFirstVisibleItemPosition();
+                int[] lastVisibleItemPositions = ((StaggeredGridLayoutManager)mLayoutManager).FindLastVisibleItemPositions(null);
+                // get maximum element within the list
+                lastVisibleItemPosition = GetLastVisibleItem(lastVisibleItemPositions);
+            }
+            else if (mLayoutManager is GridLayoutManager)
+            {
+                lastVisibleItemPosition = ((GridLayoutManager)mLayoutManager).FindLastVisibleItemPosition();
+            }
+            else if (mLayoutManager is LinearLayoutManager)
+            {
+                lastVisibleItemPosition = ((LinearLayoutManager)mLayoutManager).FindLastVisibleItemPosition();
+            }
 
-                if (totalItemCount >= previousTotal && (totalItemCount - visibleItemCount) <= (firstVisibleItemPosition + visibleThreshold))
+            // If the total item count is zero and the previous isn't, assume the
+            // list is invalidated and should be reset back to initial state
+            if (totalItemCount < previousTotalItemCount)
+            {
+                this.currentPage = this.startingPageIndex;
+                this.previousTotalItemCount = totalItemCount;
+                if (totalItemCount == 0)
                 {
-                    // End has been reached
-                    // Do something
-                    previousTotal = totalItemCount;
-                    loadMoreAction?.Invoke();
+                    this.loading = true;
                 }
             }
+            // If it’s still loading, we check to see if the dataset count has
+            // changed, if so we conclude it has finished loading and update the current page
+            // number and total item count.
+            if (loading && (totalItemCount > previousTotalItemCount))
+            {
+                loading = false;
+                previousTotalItemCount = totalItemCount;
+            }
+
+            // If it isn’t currently loading, we check to see if we have breached
+            // the visibleThreshold and need to reload more data.
+            // If we do need to reload some more data, we execute onLoadMore to fetch the data.
+            // threshold should reflect how many total columns there are too
+            if (!loading && (lastVisibleItemPosition + visibleThreshold) > totalItemCount)
+            {
+                currentPage++;
+                OnLoadMore(currentPage, totalItemCount, recyclerView);
+                loading = true;
+            }
         }
+
+        public void ResetState()
+        {
+            this.currentPage = this.startingPageIndex;
+            this.previousTotalItemCount = 0;
+            this.loading = true;
+        }
+
+        // Defines the process for actually loading more data based on page
+        public abstract void OnLoadMore(int page, int totalItemsCount, RecyclerView view);
+    }
+
+    public class EndlessScroll : EndlessRecyclerViewScrollListener
+    {
+        public event EventHandler LoadMoreTask = delegate { };
+
+        public EndlessScroll(LinearLayoutManager layoutManager) : base(layoutManager)
+        {
+
+        }
+
+        public override void OnLoadMore(int page, int totalItemsCount, RecyclerView view)
+        {
+            LoadMoreTask?.Invoke(this, null); 
+        }
+
+
     }
 }
