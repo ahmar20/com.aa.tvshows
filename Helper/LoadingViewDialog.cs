ae42678a8e8e8fdd2ad6cbc5aaac3574b245de37
Ahmar Aftab <pakahmar@hotmail.com>
2019-12-27 23:46:16 +0500

    Add singleton LoadingView dialog for URL deciphering loading

---
+++ Helper/LoadingViewDialog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using AndroidX.AppCompat.Widget;
+
+namespace com.aa.tvshows.Helper
+{
+    public class LoadingViewDialog
+    {
+        public static LoadingViewDialog Instance { get; } = new LoadingViewDialog();
+
+        AlertDialog Dialog { get; set; }
+        AlertDialog.Builder Builder { get; set; }
+        public event EventHandler OnCancelled = delegate { };
+
+        public LoadingViewDialog()
+        {
+        }
+
+
+
+        public void Show(Context context)
+        {
+            if (Dialog != null && Dialog.IsShowing) return;
+            Builder = new AlertDialog.Builder(context).SetView(LayoutInflater.From(context).Inflate(Resource.Layout.loading_dialog, null, false));
+            Dialog = Builder.Create();
+            Dialog.SetCancelable(false);
+            Dialog.SetCanceledOnTouchOutside(false);
+            Dialog.DismissEvent += Dialog_DismissEvent;
+            Dialog.CancelEvent += Dialog_DismissEvent;
+            Dialog.Show();
+        }
+
+        private void Dialog_DismissEvent(object sender, EventArgs e)
+        {
+            OnCancelled?.Invoke(sender, e);
+        }
+
+        public void Hide()
+        {
+            if (Dialog == null) return;
+
+            if (Dialog.IsShowing)
+                Dialog.Cancel();
+        }
+    }
+}
