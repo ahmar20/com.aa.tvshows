7c2c44622760a74ed141bb8de28ef72584b8dda0
Ahmar Aftab <pakahmar@hotmail.com>
2020-02-04 19:21:45 +0500

    Implement ExoPlayer for video preview

--- PlayerActivity.cs
+++ PlayerActivity.cs
@@ -13,7 +13,6 @@ using Newtonsoft.Json;
 using System.Linq;
 using Android.Views;
 
-/*
 using Com.Google.Android.Exoplayer2.Source.Dash;
 using Com.Google.Android.Exoplayer2.Source.Smoothstreaming;
 using Com.Google.Android.Exoplayer2.Source.Dash.Manifest;
@@ -27,27 +26,23 @@ using Com.Google.Android.Exoplayer2.Source.Hls;
 using Com.Google.Android.Exoplayer2.Source.Hls.Playlist;
 using Com.Google.Android.Exoplayer2.Util;
 using Com.Google.Android.Exoplayer2.Trackselection;
-*/
+
 
 namespace com.aa.tvshows
 {
     [Activity(Label = "PlayerActivity", ScreenOrientation = Android.Content.PM.ScreenOrientation.SensorLandscape, ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
-    public class PlayerActivity : AppCompatActivity
+    public class PlayerActivity : AppCompatActivity, PlayerControlView.IVisibilityListener
     {
-        //SimpleExoPlayer player;
-        //PlayerView playerView;
-        //AppCompatSpinner linksSpinner;
+        SimpleExoPlayer player;
+        PlayerView playerView;
+        AppCompatSpinner linksSpinner;
 
         List<StreamingUri> mediaStreams;
-        VideoView videoPlayer;
-        MediaController controller;
-        ContentLoadingProgressBar videoLoading;
         bool isSystemUIVisible = true;
-        int videoControllerTimeout = 7000;
-
-        //IDataSourceFactory mediaSourceFactory;
-        //TrackSelector trackSelector;
-        //readonly IBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        
+        IDataSourceFactory mediaSourceFactory;
+        ITrackSelectionFactory trackSelectorFactory;
+        readonly IBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -62,24 +57,29 @@ namespace com.aa.tvshows
         private void InitializePlayback(StreamingUri itemToPlay)
         {
             if (mediaStreams == null)
-                mediaStreams = JsonConvert.DeserializeObject<List<StreamingUri>>(Intent.GetStringExtra("mediaStreams"));
-            /*
-            if (player is null) // if player is null
+                mediaStreams = JsonConvert.DeserializeObject<List<StreamingUri>>(Intent.GetStringExtra("mediaStreams"))
+                    .Where(a => a.StreamingUrl.OriginalString.Contains(".mp4")).ToList();
+            
+            if (player is null)
             {
-                trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory());
                 mediaSourceFactory = new DefaultDataSourceFactory(this, Util.GetUserAgent(this, Application.PackageName));
-                player = ExoPlayerFactory.NewSimpleInstance(this, trackSelector);
+                trackSelectorFactory = new AdaptiveTrackSelection.Factory();
+                var renderersFactory = new DefaultRenderersFactory(this);
+                player = ExoPlayerFactory.NewSimpleInstance(this, renderersFactory, new DefaultTrackSelector(trackSelectorFactory));
                 playerView = FindViewById<PlayerView>(Resource.Id.playerView);
+                playerView.Player = player;
+                playerView.RequestFocus();
+                playerView.SetControllerVisibilityListener(this);
                 linksSpinner = FindViewById<AppCompatSpinner>(Resource.Id.linksSpinner);
 
                 player.PlayWhenReady = true;
-                playerView.Player = player;
                 playerView.ControllerShowTimeoutMs = 3000;
-                playerView.UseArtwork = true;
                 
-                if (mediaStreams != null)
+                if (mediaStreams != null && mediaStreams.Count > 0)
                 {
-                    linksSpinner.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, mediaStreams);
+                    var titles = new List<string>() { };
+                    mediaStreams.ForEach(a => titles.Add(a.StreamingQuality + " - MP4"));
+                    linksSpinner.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, titles);
                     linksSpinner.ItemSelected += LinksSpinner_ItemSelected;
                     linksSpinner.SetSelection(0);
                 }
@@ -87,98 +87,55 @@ namespace com.aa.tvshows
             if (itemToPlay != null)
             {
                 player.Prepare(CreateMediaSource(itemToPlay));
-            }
-            */
-            if (videoPlayer is null)
-            {
-                videoPlayer = FindViewById<VideoView>(Resource.Id.videoPlayer);
-                videoPlayer.SetZOrderOnTop(true);
-
-                videoPlayer.Touch += VideoPlayer_Touch;
-                videoPlayer.Prepared += VideoPlayer_Prepared;
-                videoPlayer.Error += VideoPlayer_Error;
-
-                controller = new Android.Widget.MediaController(this);
-                videoPlayer.SetMediaController(controller);
-                videoPlayer.SetLayerType(Android.Views.LayerType.Software, null);
-                videoLoading = FindViewById<ContentLoadingProgressBar>(Resource.Id.videoLoading);
-                if (itemToPlay is null)
-                {
-                    if (!(mediaStreams is null))
-                    {
-                        InitializePlayback(mediaStreams.Where(a => a.StreamingUrl.OriginalString.Contains(".mp4")).FirstOrDefault());
-                        return;
-                    }
-                }
-            }
-            
-            if (!(itemToPlay is null))
-            {
-                if(itemToPlay.StreamingUrl != null)
-                {
-                    videoLoading.Visibility = Android.Views.ViewStates.Visible;
-                    videoPlayer.SetVideoURI(Android.Net.Uri.Parse(itemToPlay.StreamingUrl.OriginalString));
-                }
-            }
-            else
-            {
-                Toast.MakeText(this, "No streamable link found.", ToastLength.Long).Show();
-                OnBackPressed();
+                playerView.KeepScreenOn = true;
             }
         }
 
-        private void VideoPlayer_Touch(object sender, Android.Views.View.TouchEventArgs e)
+        public override bool OnTouchEvent(MotionEvent e)
         {
-            if (e.Event.Action == MotionEventActions.Down)
+            if (e.Action == MotionEventActions.Down || e.Action == MotionEventActions.HoverEnter)
             {
-                e.Handled = true;
                 if (isSystemUIVisible)
                 {
-                    HideSoftwareMenuBars();
-                    if (controller.IsShowing) controller.Hide();
+                    playerView.HideController();
                 }
                 else
                 {
-                    controller.Show(videoControllerTimeout);
+                    playerView.ShowController();
                 }
+                return true;
             }
-            else
-            {
-                e.Handled = false;
-            }
+            return base.OnTouchEvent(e);
         }
 
         private void VideoPlayer_SystemUiVisibilityChange(object sender, Android.Views.View.SystemUiVisibilityChangeEventArgs e)
         {
             isSystemUIVisible = e.Visibility == Android.Views.StatusBarVisibility.Visible ? true : false;
+            if (isSystemUIVisible)
+            {
+                if (!playerView.IsControllerVisible) playerView.ShowController();
+            }
+            else
+            {
+                if (playerView.IsControllerVisible) playerView.HideController();
+            }
         }
 
-        private void VideoPlayer_Error(object sender, Android.Media.MediaPlayer.ErrorEventArgs e)
-        {
-            Toast.MakeText(this, "Video error: " + e.What.ToString(), ToastLength.Long).Show();
-            videoLoading.Visibility = Android.Views.ViewStates.Gone;
-        }
-
-        private void VideoPlayer_Prepared(object sender, EventArgs e)
-        {
-            videoLoading.Visibility = Android.Views.ViewStates.Gone;
-            videoPlayer.Start();
-            HideSoftwareMenuBars();
-        }
-
-        /*
         private IMediaSource CreateMediaSource(StreamingUri link)
         {
             Android.Net.Uri uri = Android.Net.Uri.Parse(link.StreamingUrl.OriginalString);
             IMediaSource source = null;
             if (link.StreamingUrl.OriginalString.Contains(".mpd"))
             {
-                var dashFactory = new DefaultDashChunkSource.Factory(mediaSourceFactory);
-                source = new DashMediaSource.Factory(dashFactory, mediaSourceFactory).CreateMediaSource(uri);
+                source = new DashMediaSource.Factory(new DefaultDashChunkSource.Factory(mediaSourceFactory), mediaSourceFactory)
+                        .SetManifestParser(new FilteringManifestParser(new DashManifestParser(), new List<StreamKey>()))
+                        .CreateMediaSource(uri);
             }
             if (link.StreamingUrl.OriginalString.Contains(".m3u8"))
             {
-                source = new HlsMediaSource.Factory(mediaSourceFactory).CreateMediaSource(uri);
+                source = new HlsMediaSource.Factory(mediaSourceFactory)
+                        .SetPlaylistParserFactory(new DefaultHlsPlaylistParserFactory(new List<StreamKey>()))
+                        .CreateMediaSource(uri);
             }
             if (link.StreamingUrl.OriginalString.Contains(".mp4"))
             {
@@ -186,8 +143,7 @@ namespace com.aa.tvshows
             }
             return source;
         }
-        */
-
+        
         private void LinksSpinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
         {
             var item = mediaStreams[e.Position];
@@ -197,28 +153,51 @@ namespace com.aa.tvshows
         protected override void OnPause()
         {
             base.OnPause();
-            if (videoPlayer != null && videoPlayer.CanPause())
-                videoPlayer.Pause();
-            //player.PlayWhenReady = false;
+            if (player != null)
+                player.PlayWhenReady = false;
         }
 
         public override void OnBackPressed()
         {
             base.OnBackPressed();
-            videoPlayer.StopPlayback();
-            videoPlayer.Dispose();
-            videoPlayer = null;
+            player.PlayWhenReady = false;
+            player.Release();
+            playerView.KeepScreenOn = false;
+            player = null;
         }
 
         private void HideSoftwareMenuBars()
         {
             int uiOptions = (int)Window.DecorView.SystemUiVisibility;
-            uiOptions |= (int)SystemUiFlags.HideNavigation;
-            uiOptions |= (int)SystemUiFlags.Fullscreen;
-            uiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+            uiOptions |= (int)SystemUiFlags.LowProfile;
+            uiOptions |= (int)SystemUiFlags.LayoutFullscreen | (int)SystemUiFlags.Fullscreen;
+            uiOptions |= (int)SystemUiFlags.LayoutHideNavigation | (int)SystemUiFlags.HideNavigation;
 
             Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
             isSystemUIVisible = false;
+        }
+
+        private void ShowSoftwareMenuBars()
+        {
+            int uiOptions = (int)Window.DecorView.SystemUiVisibility;
+            uiOptions |= (int)SystemUiFlags.Visible;
+            uiOptions |= (int)SystemUiFlags.LayoutFullscreen;
+            uiOptions |= (int)SystemUiFlags.LayoutHideNavigation;
+
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)uiOptions;
+            isSystemUIVisible = true;
+        }
+
+        public void OnVisibilityChange(int visibility)
+        {
+            if ((ViewStates)visibility == ViewStates.Gone || (ViewStates)visibility == ViewStates.Invisible)
+            {
+                HideSoftwareMenuBars();
+            }
+            else
+            {
+                ShowSoftwareMenuBars();
+            }
         }
     }
 }
